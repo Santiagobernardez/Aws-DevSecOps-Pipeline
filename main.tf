1701cb2e3f6921d2019fe3c531a768b14087e6cf
santibernardez <santibernardez9@gmail.com>
2026-02-26 19:33:19 +0000

    feat: write IaC to provision and teardown AWS EC2 instance

---
+++ main.tf
@@ -0,0 +1,33 @@
+#Pinning to Ubuntu 22.04 LTS as the base image for stability and long-term security patching
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = "true"
+  owners = ["099720109477"]
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+
+resource "aws_instance" "Web_server" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro" # Kept on t3.micro to stay within AWS Free Tier
+  tags = {
+    Name = "DevSecOps-Server"
+  }
+}
